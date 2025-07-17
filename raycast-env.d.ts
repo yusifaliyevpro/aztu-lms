752e7f9e2aa81ee60055ca1aeed1a0f6a861b0aa
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-07-17 22:39:37 +0400

    First Commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** AzTU LMS username - Write your AzTU LMS username */
+  "username": string,
+  /** AzTU LMS password - Write your AzTU LMS password */
+  "password": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `login-account` command */
+  export type LoginAccount = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `login-account` command */
+  export type LoginAccount = {}
+}
+

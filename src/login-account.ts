a7fb84d1592601ae4c7ca5427c6f9c1cc1078bfb
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-09-20 22:16:30 +0400

    fix: streamline login error handling and improve toast notifications

--- src/login-account.ts
+++ src/login-account.ts
@@ -1,22 +1,14 @@
 import { closeMainWindow, open, showToast, Toast } from "@raycast/api";
-import { getSSOUrl } from "./lib/login";
+import { getSSOUrl } from "@/lib/login";
 
 export default async function Command() {
-    const loadingToast = await showToast({ title: "Logging Account...", style: Toast.Style.Animated });
-
-    try {
-        const data = await getSSOUrl({ forLogin: true });
-        if (data) {
-            if (data.status === "new") await open(data.loginLink);
-            else await open("https://lms.aztu.edu.az");
-        } else {
-            throw new Error(data);
-        }
-    } catch (error) {
-        console.log(error);
-        await showToast({ title: "Failed to get Login URL", message: "Try again!" });
+    const data = await getSSOUrl({ forLogin: true });
+    if (data) {
+        if (data.status === "new") await open(data.loginLink);
+        else await open("https://lms.aztu.edu.az");
+    } else {
+        await showToast({ title: "Failed to get Login URL", style: Toast.Style.Failure });
     }
 
-    await loadingToast.hide();
     await closeMainWindow({ clearRootSearch: true });
 }
